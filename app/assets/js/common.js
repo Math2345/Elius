14bc621437c2cbda11411c9513cda2d95e12aa3f
Maxim <maxim.hramtsoff@gmail.com>
2022-07-13 11:26:23 +0300

    start layout desktop

---
+++ app/assets/js/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.card-rainbow-slider').slick({
+      dots:true,
+      arrow: true,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1
+    });  
+});
